a0ca4a77819dbb4fd9a3d28ac376ac7e02237138
Jecheon Ryu <rjc1704@gmail.com>
2023-07-03 09:40:27 +0900

    test

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import store from "./redux/config/configStore";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+//
 root.render(
   <React.StrictMode>
     <Provider store={store}>
